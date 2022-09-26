7597382c54837e25915f20d54f367e52554631a9
BetterCallmeSaul <114093431+BetterCallmeSaul@users.noreply.github.com>
2022-09-26 18:39:55 +0300

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+using namespace std;
+int main() {
+	
+    /* 	int a, b, c;
+	int k = 0;
+	cin >> a >> b >> c;
+	if (a > 0) {
+		k++;
+	}
+	if (b > 0) {
+		k++;
+	}
+	if (c > 0) {
+		k++;
+	}
+    cout << k << endl;*/
+	/* 
+	int a, b;
+	cin >> a >> b;
+	if (a > b) {
+		cout << a << endl;
+
+	}
+	else if (b > a) {
+		cout << b << endl;
+
+	}
+	else if (a == b) {
+		cout << "equals" << endl;
+	}*/
+	/* 
+	//If7.Даны два числа.Вывести порядковый номер меньшего из них.
+	int a, b;
+	cin >> a >> b;
+	if (a > b) {
+		cout << 1 << endl;
+	}
+	if (b == a) {
+		cout << 2 << endl;
+	}*/
+}
